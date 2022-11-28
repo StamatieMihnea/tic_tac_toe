62ccd568e714fe5ffa38a0cbe52e569afd522d7c
Mihnea <stamatiemihnea@gmail.com>
2022-11-28 17:12:10 +0200

    Create game layout and logic Update README.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,224 @@
 import 'package:flutter/material.dart';
 
+const int squareSide = 3;
+
+///First color for empty cell, Second color for Player 1 cell, Third color for Player 2 cell
+const List<Color> usedColors = <Color>[Colors.white, Colors.green, Colors.red];
+const List<List<int>> emptyScoreMatrix = <List<int>>[
+  <int>[0, 0, 0],
+  <int>[0, 0, 0],
+  <int>[0, 0, 0]
+];
+
 void main() {
-  runApp(const MyApp());
+  runApp(const TicTacToeGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TicTacToeGame extends StatelessWidget {
+  const TicTacToeGame({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: const ColorScheme.light(primary: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GameMainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GameMainScreen extends StatefulWidget {
+  const GameMainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameMainScreen> createState() => _GameMainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameMainScreenState extends State<GameMainScreen> {
+  List<List<int>> scoredCells = <List<int>>[
+    <int>[0, 0, 0],
+    <int>[0, 0, 0],
+    <int>[0, 0, 0]
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  int currentPlayer = 1;
+  int filledCellsCount = 0;
+
+  ///There are 3 lines in the grid without the "Reset" button and 4 lines with it
+  int numberOfLines = squareSide;
+
+  void resetCells() {
+    scoredCells = <List<int>>[
+      <int>[0, 0, 0],
+      <int>[0, 0, 0],
+      <int>[0, 0, 0]
+    ];
+  }
+
+  void changePlayer() {
+    if (currentPlayer == 1) {
+      currentPlayer = 2;
+    } else {
+      currentPlayer = 1;
+    }
+  }
+
+  void checkMatrix() {
+    ///check lines
+    for (int i = 0; i < squareSide; i++) {
+      bool flag = true;
+      int winner = 0;
+      for (int j = 0; j < squareSide - 1; j++) {
+        winner = scoredCells[i][j];
+        if (scoredCells[i][j] != scoredCells[i][j + 1] || scoredCells[i][j] == 0) {
+          flag = false;
+        }
+      }
+      if (flag) {
+        setState(() {
+          resetCells();
+          for (int k = 0; k < squareSide; k++) {
+            scoredCells[i][k] = winner;
+          }
+          numberOfLines = 4;
+        });
+        return;
+      }
+    }
+
+    ///check columns
+    for (int j = 0; j < squareSide; j++) {
+      bool flag = true;
+      int winner = 0;
+      for (int i = 0; i < squareSide - 1; i++) {
+        winner = scoredCells[i][j];
+        if (scoredCells[i][j] != scoredCells[i + 1][j] || scoredCells[i][j] == 0) {
+          flag = false;
+        }
+      }
+      if (flag) {
+        setState(() {
+          resetCells();
+          for (int k = 0; k < squareSide; k++) {
+            scoredCells[k][j] = winner;
+          }
+          numberOfLines = 4;
+        });
+        return;
+      }
+    }
+
+    ///check main diagonal
+    bool flag = true;
+    int winner = 0;
+    for (int i = 0; i < squareSide - 1; i++) {
+      winner = scoredCells[i][i];
+      if (scoredCells[i][i] != scoredCells[i + 1][i + 1] || scoredCells[i][i] == 0) {
+        flag = false;
+      }
+    }
+    if (flag) {
+      setState(() {
+        resetCells();
+        for (int k = 0; k < squareSide; k++) {
+          scoredCells[k][k] = winner;
+        }
+        numberOfLines = 4;
+      });
+      return;
+    }
+
+    ///check main diagonal
+    bool flagSecond = true;
+    int winnerSecond = 0;
+    for (int i = 0; i < squareSide - 1; i++) {
+      winnerSecond = scoredCells[i][squareSide - i - 1];
+      if (scoredCells[i][squareSide - i - 1] != scoredCells[i + 1][squareSide - i - 2] ||
+          scoredCells[i][squareSide - i - 1] == 0) {
+        flagSecond = false;
+      }
+    }
+    if (flagSecond) {
+      setState(() {
+        resetCells();
+        for (int k = 0; k < squareSide; k++) {
+          scoredCells[k][squareSide - k - 1] = winnerSecond;
+        }
+        numberOfLines = 4;
+      });
+      return;
+    }
+
+    ///check if it is tie and all cells are checked
+    if (filledCellsCount == squareSide * squareSide) {
+      setState(() {
+        numberOfLines = 4;
+      });
+    }
+  }
+
+  List<Widget> generateGridElements() {
+    return List<Widget>.generate(squareSide * numberOfLines, (int index) {
+      if (index < squareSide * squareSide) {
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              if (scoredCells[index ~/ 3][index % 3] == 0) {
+                scoredCells[index ~/ 3][index % 3] = currentPlayer;
+                filledCellsCount++;
+                changePlayer();
+                checkMatrix();
+              }
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            color: usedColors[scoredCells[index ~/ 3][index % 3]],
+          ),
+        );
+      } else {
+        ///If the "Reset" button is needed a row wth two colored box and the button between is added
+        if (index == squareSide * squareSide + 1) {
+          return Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  numberOfLines = squareSide;
+                  resetCells();
+                  filledCellsCount = 0;
+                });
+              },
+              child: const Text('Reset'),
+            ),
+          );
+        } else {
+          return const ColoredBox(
+            color: Colors.black,
+          );
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: const Center(
+          child: Text('Tic Tac Toe'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ColoredBox(
+        color: Colors.black,
+        child: GridView.count(
+          crossAxisCount: squareSide,
+          padding: const EdgeInsets.all(3),
+          mainAxisSpacing: 3,
+          crossAxisSpacing: 3,
+          children: generateGridElements(),
+        ),
+      ),
     );
   }
 }
